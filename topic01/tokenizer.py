522a88c052a8a183938064e9816e9a4879650bb6
Yury Sergeev <yu.m.sergeev@gmail.com>
2025-08-20 21:10:16 -0400

    x
    c

--- topic01/tokenizer.py
+++ topic01/tokenizer.py
@@ -1,16 +1,17 @@
-# tokenizer.py 
-# Created: 8/20/2025
-# Last Edit: 8/20/2025
+# tokenizer.py
 # Yury Sergeev
+# Created: 8/20/2025
+# Updated: 8/20/2025
+
+# This is a tokenizer for a custom programming language built in class/as an assignment
+# for CS33101-Structure Of Programming Languages
 
 import re
 
 patterns = [
-
-    [r"\d*\.\d+|\d\.\d*|\d+", "number"], #int, float, and int dot (1, 1.123, 1.)
-    [r"\+", "+"],                        # + sign recognition
-    [r".","error"]                       #Nothing matched therfore error
-
+    [r"\d*\.\d+|\d+\.\d*|\d+","number"],  # int, float.nums, float.none
+    [r"\+", "+"],                         # plus (+)
+    [r".","error"]                        # none found -> error
 ]
 
 for pattern in patterns:
@@ -20,7 +21,7 @@ def tokenize(characters):
     tokens = []
     position = 0
     while position < len(characters):
-        #find first token match
+        # find first matching token
         for pattern, tag in patterns:
             match = pattern.match(characters, position)
             if match:
@@ -28,17 +29,16 @@ def tokenize(characters):
 
         assert match
 
-        if tag =="error":
-            raise Exception(f"Syntax Error: illegal char:{[match.group(0)]}")
-
+        if tag == "error":
+            raise Exception(f"Syntax error: illegal character :{[match.group(0)]}")
+    
         token = {"tag":tag, "position":position}
         value = match.group(0)
         if token["tag"] == "number":
             if "." in value:
-                token["value"] == float(value)
-            else: 
-                token["value"] == int(value)
-
+                token["value"] = float(value)
+            else:
+                token["value"] = int(value)
         tokens.append(token)
         position = match.end()
 
@@ -46,24 +46,25 @@ def tokenize(characters):
     return tokens
 
 def test_simple_tokens():
-    print("test_simple_tokens...")
-    assert tokenize("+") == [ 
+
+    print("Testing simple tokens... \n.\n.\n.")
+
+    # Test + operator
+    assert tokenize("+") == [
         {"tag":"+", "position":0},
         {"tag":None, "position":1}
     ]
+    print("+ operator - Pass")
 
-    print("test_simple_token...")
-    assert tokenize("3") == [ 
-        {"tag":"number", "position":0, "value":3}
+    # Test int 
+    assert tokenize("3") == [
+        {"tag":"number", "position":0, "value":3},
         {"tag":None, "position":1}
     ]
+    print("integer - Pass")
 
 
 if __name__ == "__main__":
-
-    print("Testing the Tokenizer...")
-
+    print("testing tokenizer...")
     test_simple_tokens()
-
-
-    print("Done testing Tonkenizer.")
+    print("done.")
